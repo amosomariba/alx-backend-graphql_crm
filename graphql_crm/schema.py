a196c3a1a63265601d3b1944babb96bb77217a28
Amos Omariba <amomariba@gmail.com>
2025-07-02 14:15:17 +0300

    Set Up GraphQL Endpoint

--- graphql_crm/schema.py
+++ graphql_crm/schema.py
@@ -1,9 +1,10 @@
 import graphene
+from crm.schema import Query as CrmQuery, Mutation as CrmMutation
 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
+class Query(CrmQuery, graphene.ObjectType):
+    pass
 
-    def resolve_hello(root, info):
-        return "Hello, GraphQL!"
+class Mutation(CrmMutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
