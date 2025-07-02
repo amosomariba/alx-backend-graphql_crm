4d757b7269f97d22922a609fd3ca13d5e518d271
Amos Omariba <amomariba@gmail.com>
2025-07-02 17:04:29 +0300

    Add Filtering

--- crm/schema.py
+++ crm/schema.py
@@ -7,6 +7,13 @@ from .types import CustomerType, ProductType, OrderType
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
+from .filters import CustomerFilter, ProductFilter, OrderFilter
+
+class Query(graphene.ObjectType):
+    all_customers = DjangoFilterConnectionField(CustomerType, filterset_class=CustomerFilter)
+    all_products = DjangoFilterConnectionField(ProductType, filterset_class=ProductFilter)
+    all_orders = DjangoFilterConnectionField(OrderType, filterset_class=OrderFilter)
 
 
 class CreateCustomer(graphene.Mutation):
--- crm/types.py
+++ crm/types.py
@@ -5,13 +5,18 @@ from .models import Customer, Product, Order
 class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
-        fields = ("id", "name", "email", "phone")
-
+        interfaces = (graphene.relay.Node,)
+        fields = ("id", "name", "email", "phone", "created_at")
 
 class ProductType(DjangoObjectType):
     class Meta:
         model = Product
+        interfaces = (graphene.relay.Node,)
+        fields = ("id", "name", "price", "stock")
 
 class OrderType(DjangoObjectType):
     class Meta:
         model = Order
+        interfaces = (graphene.relay.Node,)
+        fields = ("id", "customer", "products", "total_amount", "order_date")
+
